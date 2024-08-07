895b131633f9d88b931fd36108a735183bf50fc2
Abishek759 <abiabishek836@gmail.com>
2024-08-07 22:11:18 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ToastProvider } from "react-toast-notifications";
+import "./Styles/index.css";
+import App from "./components/App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root rendering 
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ToastProvider autoDismiss autoDismissTimeout={5000} placement="top-left">
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ToastProvider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
